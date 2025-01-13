0375b932787f438d6e395525cc36a01658b38688
alicia <smushyaa@gmail.com>
2025-01-13 16:49:56 -0500

    string:cpp: function overloads that automatically cast C++ strings to C POD strings

--- core/cpp/string.hpp
+++ core/cpp/string.hpp
@@ -27,17 +27,22 @@ usize __strlen( const char* cstr ) {
 struct _StringCPP {
     typedef char Type;
 
-    /// @brief Byte length of string.
-    usize len = 0;
     union {
-        /// @brief Pointer to string buffer.
-        const Type* cbuf = nullptr;
-        /// @brief Constant pointer to string buffer.
-        Type*       buf;
-        /// @brief Pointer to string bytes.
-        u8*         bytes;
-        /// @brief Void pointer to string buffer.
-        void*       _void;
+        struct {
+            /// @brief Byte length of string.
+            usize len = 0;
+            union {
+                /// @brief Pointer to string buffer.
+                const Type* cbuf = nullptr;
+                /// @brief Constant pointer to string buffer.
+                Type*       buf;
+                /// @brief Pointer to string bytes.
+                u8*         bytes;
+                /// @brief Void pointer to string buffer.
+                void*       _void;
+            };
+        };
+        _StringPOD pod;
     };
 
     /// @brief Create empty string.
@@ -97,31 +102,46 @@ attr_header
 bool operator!=( const _StringPOD& a, const _StringPOD& b ) {
     return !( a == b );
 }
+attr_header
+bool operator==( const _StringCPP& a, const _StringCPP& b ) {
+    return string_cmp( a.pod, b.pod );
+}
+attr_header
+bool operator!=( const _StringCPP& a, const _StringCPP& b ) {
+    return !( a == b );
+}
 
 struct _StringBufCPP {
     typedef char Type;
     
-    /// @brief Byte capacity of string buffer.
-    usize cap = 0;
     union {
         struct {
-            /// @brief Byte length of string.
-            usize len = 0;
+            /// @brief Byte capacity of string buffer.
+            usize cap = 0;
             union {
-                /// @brief Constant pointer to string buffer.
-                const Type* cbuf = nullptr;
-                /// @brief Pointer to string buffer.
-                Type*       buf;
-                /// @brief Pointer to string bytes.
-                u8*         bytes;
-                /// @brief Void pointer to string buffer.
-                void*       _void;
+                struct {
+                    /// @brief Byte length of string.
+                    usize len = 0;
+                    union {
+                        /// @brief Constant pointer to string buffer.
+                        const Type* cbuf = nullptr;
+                        /// @brief Pointer to string buffer.
+                        Type*       buf;
+                        /// @brief Pointer to string bytes.
+                        u8*         bytes;
+                        /// @brief Void pointer to string buffer.
+                        void*       _void;
+                    };
+                };
+                /// @brief String buffer as a slice.
+                _StringCPP  string;
+                /// @brief String buffer as a slice.
+                Slice<char> slice;
+
+                _StringPOD  string_pod;
             };
         };
-        /// @brief String buffer as a slice.
-        _StringCPP  slice;
-        /// @brief String buffer as a slice.
-        Slice<char> _slice;
+        _StringBufPOD pod;
     };
 
     /// @brief Create empty string buffer.
@@ -191,7 +211,7 @@ struct _StringBufCPP {
     /// @return Slice.
     attr_header constexpr
     operator Slice<Type>() const {
-        return _slice;
+        return slice;
     }
 
     attr_always_inline attr_header attr_optimized constexpr
@@ -209,6 +229,216 @@ bool operator==( const _StringBufPOD& a, const _StringBufPOD& b ) {
 attr_header
 bool operator!=( const _StringBufPOD& a, const _StringBufPOD& b ) {
     return !( a == b );
+}
+attr_header
+bool operator==( const _StringBufCPP& a, const _StringBufCPP& b ) {
+    return string_cmp( a.string_pod, b.string_pod );
+}
+attr_header
+bool operator!=( const _StringBufCPP& a, const _StringBufCPP& b ) {
+    return !( a == b );
+}
+
+/// @brief Split string at given index.
+/// @details
+/// Debug asserts that @c at is <= source length.
+/// @param      source        String to split from.
+/// @param      at            Index to split at. Character at this index is not included in splits.
+/// @param[out] opt_out_left  (optional) Pointer to write left side of split.
+/// @param[out] opt_out_right (optional) Pointer to write right side of split.
+attr_always_inline attr_header 
+void string_split(
+    struct _StringCPP source, usize at,
+    struct _StringCPP* opt_out_left, struct _StringCPP* opt_out_right
+) {
+    return string_split(
+        source.pod, at, (_StringPOD*)opt_out_left, (_StringPOD*)opt_out_right );
+}
+/// @brief Split string at first instance of given ascii character.
+/// @details
+/// Debug asserts that @c at is <= source length.
+/// @param      source        String to split from.
+/// @param      c             Character to split at. Character at this index is not included in splits.
+/// @param[out] opt_out_left  (optional) Pointer to write left side of split.
+/// @param[out] opt_out_right (optional) Pointer to write right side of split.
+/// @return
+///     - @c true  : @c c was found in @c source.
+///     - @c false : @c c was not found, string was not split.
+attr_always_inline attr_header
+b32 string_split_ascii(
+    struct _StringCPP source, char c,
+    struct _StringCPP* opt_out_left, struct _StringCPP* opt_out_right
+) {
+    return string_split_ascii(
+        source.pod, c,
+        (_StringPOD*)opt_out_left, (_StringPOD*)opt_out_right );
+}
+/// @brief Split string at first instance of whitespace character.
+/// @details
+/// Debug asserts that @c at is <= source length.
+/// Trims whitespace from results.
+/// @param      source        String to split from.
+/// @param[out] opt_out_left  (optional) Pointer to write left side of split.
+/// @param[out] opt_out_right (optional) Pointer to write right side of split.
+/// @return
+///     - @c true  : @c c was found in @c source.
+///     - @c false : @c c was not found, string was not split.
+attr_always_inline attr_header
+b32 string_split_whitespace(
+    struct _StringCPP source,
+    struct _StringCPP* opt_out_left,
+    struct _StringCPP* opt_out_right
+) {
+    return string_split_whitespace(
+        source.pod,
+        (_StringPOD*)opt_out_left,
+        (_StringPOD*)opt_out_right );
+}
+
+/// @brief Calculate remaining space in string buffer.
+/// @param[in] buf String buffer.
+/// @return Number of bytes (characters) remaining in string.
+attr_always_inline attr_header
+usize string_buf_remaining( const struct _StringBufCPP* buf ) {
+    return string_buf_remaining( (_StringBufPOD*)buf );
+}
+/// @brief Check if string buffer is empty.
+/// @param[in] buf Pointer to string buffer.
+/// @return
+///     - @c true  : String buffer is empty.
+///     - @c false : String buffer is not empty.
+attr_always_inline attr_header
+b32 string_buf_is_empty( const struct _StringBufCPP* buf ) {
+    return string_buf_is_empty( (_StringBufPOD*)buf );
+}
+/// @brief Check if string buffer is full.
+/// @note String buffer always tries to have space for null terminator.
+/// @param[in] buf Pointer to string buffer.
+/// @return
+///     - @c true  : String buffer is full.
+///     - @c false : String buffer still has space.
+attr_always_inline attr_header
+b32 string_buf_is_full( const struct _StringBufCPP* buf ) {
+    return string_buf_is_full( (_StringBufPOD*)buf );
+}
+/// @brief Set string buffer length to zero and zero out memory.
+/// @param[in] buf Pointer to string buffer.
+attr_always_inline attr_header
+void string_buf_clear( struct _StringBufCPP* buf ) {
+    string_buf_clear( (_StringBufPOD*)buf );
+}
+/// @brief Attempt to push character to end of string buffer.
+/// @param[in] buf String buffer to push character to.
+/// @param     c   Character to push.
+/// @return
+///     - @c true  : @c buf had enough space to push character.
+///     - @c false : @c buf is full.
+attr_always_inline attr_header
+b32 string_buf_try_push( struct _StringBufCPP* buf, char c ) {
+    return string_buf_try_push( (_StringBufPOD*)buf, c );
+}
+/// @brief Attempt to emplace a character inside of string buffer.
+/// @param[in] buf String buffer to emplace character in.
+/// @param     c   Character to emplace.
+/// @param     at  Index to emplace character at.
+/// @return
+///     - @c true  : @c buf had enough space to emplace character.
+///     - @c false : @c buf is full.
+attr_always_inline attr_header
+b32 string_buf_try_emplace( struct _StringBufCPP* buf, char c, usize at ) {
+    return string_buf_try_emplace( (_StringBufPOD*)buf, c, at );
+}
+/// @brief Pop character from end of string buffer.
+/// @param[in]  buf       String buffer to pop from.
+/// @param[out] opt_out_c (optional) Pointer to write popped character to.
+/// @return
+///     - @c true  : @c buf had character to pop.
+///     - @c false : @c buf was empty.
+attr_always_inline attr_header
+b32 string_buf_pop( struct _StringBufCPP* buf, char* opt_out_c ) {
+    return string_buf_pop( (_StringBufPOD*)buf, opt_out_c );
+}
+/// @brief Attempt to insert string in string buffer.
+/// @param[in] buf    String buffer to insert in.
+/// @param     insert String to insert.
+/// @param     at     Index to insert at. Must be 0 to @c buf.len
+/// @return
+///     - @c true  : @c buf had enough capacity to insert string.
+///     - @c false : @c buf did not have enough capacity to insert.
+attr_always_inline attr_header
+b32 string_buf_try_insert(
+    struct _StringBufCPP* buf, struct _StringPOD insert, usize at
+) {
+    return string_buf_try_insert( (_StringBufPOD*)buf, insert, at );
+}
+/// @brief Attempt to prepend string in string buffer.
+/// @param[in] buf     String buffer to insert in.
+/// @param     prepend String to prepend.
+/// @return
+///     - @c true  : @c buf had enough capacity to insert string.
+///     - @c false : @c buf did not have enough capacity to insert.
+attr_always_inline attr_header
+b32 string_buf_try_prepend( struct _StringBufCPP* buf, struct _StringPOD prepend ) {
+    return string_buf_try_prepend( (_StringBufPOD*)buf, prepend );
+}
+/// @brief Attempt to append string in string buffer.
+/// @param[in] buf    String buffer to insert in.
+/// @param     append String to append.
+/// @return
+///     - @c true  : @c buf had enough capacity to insert string.
+///     - @c false : @c buf did not have enough capacity to insert.
+attr_always_inline attr_header
+b32 string_buf_try_append( struct _StringBufCPP* buf, struct _StringPOD append ) {
+    return string_buf_try_append( (_StringBufPOD*)buf, append );
+}
+/// @brief Remove character from string buffer.
+/// @param[in] buf Buffer to remove character from.
+/// @param     at  Index of character to remove. Must be in bounds.
+attr_always_inline attr_header
+void string_buf_remove( struct _StringBufCPP* buf, usize at ) {
+    string_buf_remove( (_StringBufPOD*)buf, at );
+}
+/// @brief Remove range of characters from string buffer.
+/// @param[in] buf            Buffer to remove characters from.
+/// @param     from_inclusive Start of range to remove.
+/// @param     to_exclusive   End of range to remove.
+attr_always_inline attr_header
+void string_buf_remove_range(
+    struct _StringBufCPP* buf, usize from_inclusive, usize to_exclusive
+) {
+    string_buf_remove_range( (_StringBufPOD*)buf, from_inclusive, to_exclusive );
+}
+/// @brief Attempt to write a formatted string to string buffer.
+/// @param[in] buf        Buffer to write to.
+/// @param     format_len Length of format string.
+/// @param     format     Format string.
+/// @param     va         Variadic format arguments.
+/// @return
+///     - Zero   : @c buf had enough capacity to write format string.
+///     - > Zero : Number of characters not written to @c buf.
+attr_always_inline attr_header
+usize string_buf_try_fmt_buffer_va(
+    struct _StringBufCPP* buf, usize format_len, const char* format, va_list va
+) {
+    return string_buf_try_fmt_buffer_va( (_StringBufPOD*)buf, format_len, format, va );
+}
+/// @brief Attempt to write a formatted string to string buffer.
+/// @param[in] buf        Buffer to write to.
+/// @param     format_len Length of format string.
+/// @param     format     Format string.
+/// @param     ...        Format arguments.
+/// @return
+///     - Zero   : @c buf had enough capacity to write format string.
+///     - > Zero : Number of characters not written to @c buf.
+attr_always_inline attr_header
+usize string_buf_try_fmt_buffer(
+    struct _StringBufCPP* buf, usize format_len, const char* format, ...
+) {
+    va_list va;
+    va_start( va, format );
+    usize result = string_buf_try_fmt_buffer_va( (_StringBufPOD*)buf, format_len, format, va );
+    va_end( va );
+    return result;
 }
 
 #endif /* header guard */
